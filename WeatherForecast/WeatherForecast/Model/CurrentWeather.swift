9413557df65d383355bfee8b0e4ef877ff4580e0
longlivedrgn <kimye9808@naver.com>
2023-03-14 15:35:42 +0900

    feat: CurrentWeather 데이터 모델 정의

---
+++ WeatherForecast/WeatherForecast/Model/CurrentWeather.swift
@@ -0,0 +1,35 @@
+//
+//  CurrentWeather.swift
+//  WeatherForecast
+//
+//  Created by 김용재 on 2023/03/14.
+//
+
+import Foundation
+
+struct CurrentWeather: Codable {
+    let coordinate: Coordinate
+    let weather: [WeatherData]
+    
+    enum CodingKeys: String, CodingKey {
+        case coordinate = "coord"
+        case weather
+    }
+}
+
+struct Coordinate: Codable {
+    let longitude: Double
+    let latitude: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case longitude = "lon"
+        case latitude = "lat"
+    }
+}
+
+struct WeatherData: Codable {
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
