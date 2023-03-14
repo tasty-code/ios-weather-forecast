ee1ef4ed877628d1ca094769009e135d5959e75d
Mason Kim <qwerty3345@naver.com>
2023-03-14 11:38:16 +0900

    feat: 받아온 Weather 데이터 JSON 디코딩 구현

--- WeatherForecast/WeatherForecast/OpenWeatherRepository.swift
+++ WeatherForecast/WeatherForecast/OpenWeatherRepository.swift
@@ -9,16 +9,32 @@ import Foundation
 
 final class OpenWeatherRepository {
     func fetchWeather(lattitude: Double, longitude: Double) {
-        let urlString = "https://api.openweathermap.org/data/2.5/forecast?lat=\(lattitude)&lon=\(longitude)&appid=\(Bundle.main.apiKey)"
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(lattitude)&lon=\(longitude)&appid=\(Bundle.main.apiKey)"
         guard let url = URL(string: urlString) else { return }
+
+        let decoder = JSONDecoder()
         
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            guard error == nil else { return }
+            guard error == nil else {
+                // TODO: 에러 처리
+                return
+            }
+
             guard let httpResponse = response as? HTTPURLResponse,
-                  (200...299).contains(httpResponse.statusCode) else { return }
+                  (200...299).contains(httpResponse.statusCode) else {
+                // TODO: status code 에러 처리
+                return
+            }
             
             guard let data else { return }
-            print(data)
+
+            do {
+                let currentWeather = try decoder.decode(CurrentWeather.self, from: data)
+                print(currentWeather)
+            } catch {
+                // TODO: JSON 디코딩 에러 처리
+                print(error.localizedDescription)
+            }
         }
         
         task.resume()
