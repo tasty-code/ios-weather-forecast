bed480eebe7e17d987948a6c1ae9560e8f271793
Jason <ggj00125@gmail.com>
2023-03-14 16:34:11 +0900

    Feat: Wind 타입 구현

---
+++ WeatherForecast/WeatherForecast/Entity/CurrentWeather/Wind.swift
@@ -0,0 +1,17 @@
+//
+//  Wind.swift
+//  WeatherForecast
+//
+//  Created by Jason on 2023/03/14.
+//
+
+import Foundation
+
+struct Wind {
+    let speed: Double
+    let degree: Double
+    
+    private enum CodingKey: String, CodingKey {
+        case degree = "deg"
+    }
+}
