c51e3ef728e92a096895a497bd4c1a306f5b8393
Mason Kim <qwerty3345@naver.com>
2023-03-14 16:31:17 +0900

    refactor: 함수 분리 (performRequest)

--- WeatherForecast/WeatherForecast/Repository/OpenWeatherRepository.swift
+++ WeatherForecast/WeatherForecast/Repository/OpenWeatherRepository.swift
@@ -8,78 +8,55 @@
 import Foundation
 
 final class OpenWeatherRepository {
+
+    // baseURL 분리
     
-    func fetchWeather(lattitude: Double, longitude: Double, completion: @escaping (Result<CurrentWeather, NetworkError>) -> Void) {
+    func fetchWeather(lattitude: Double, longitude: Double,
+                      completion: @escaping (Result<CurrentWeather, NetworkError>) -> Void) {
         let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(lattitude)&lon=\(longitude)&appid=\(Bundle.main.apiKey)"
-        
-        guard let url = URL(string: urlString) else {
-            completion(.failure(.invalidURL))
-            return
-        }
-        
-        let decoder = JSONDecoder()
-        
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            guard error == nil else {
-                completion(.failure(.networkingError))
-                return
-            }
-            
-            guard let httpResponse = response as? HTTPURLResponse,
-                  (200...299).contains(httpResponse.statusCode) else {
-                completion(.failure(.responseError))
-                return
-            }
-            
-            guard let data else {
-                completion(.failure(.invalidData))
-                return
-            }
-
-            do {
-                let currentWeather = try decoder.decode(CurrentWeather.self, from: data)
-                completion(.success(currentWeather))
-            } catch {
-                completion(.failure(.parseError))
-            }
-        }
-        task.resume()
+        performRequest(with: urlString, completion: completion)
     }
-    
-    func fetchForecast(lattitude: Double, longitude: Double, completion: @escaping (Result<Forecast, NetworkError>) -> Void) {
+
+    func fetchForecast(lattitude: Double, longitude: Double,
+                       completion: @escaping (Result<Forecast, NetworkError>) -> Void) {
         let urlString = "https://api.openweathermap.org/data/2.5/forecast?lat=\(lattitude)&lon=\(longitude)&appid=\(Bundle.main.apiKey)"
-        
+        performRequest(with: urlString, completion: completion)
+    }
+
+    private func performRequest<T: Decodable>(with urlString: String, completion: @escaping (Result<T, NetworkError>) -> Void) {
         guard let url = URL(string: urlString) else {
             completion(.failure(.invalidURL))
             return
         }
-        
+
         let decoder = JSONDecoder()
-        
+
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             guard error == nil else {
                 completion(.failure(.networkingError))
                 return
             }
-            
+
             guard let httpResponse = response as? HTTPURLResponse,
                   (200...299).contains(httpResponse.statusCode) else {
                 completion(.failure(.responseError))
                 return
             }
-            
+
             guard let data else {
                 completion(.failure(.invalidData))
                 return
             }
 
             do {
-                let currentForecast = try decoder.decode(Forecast.self, from: data)
-                completion(.success(currentForecast))
+                let decodedData = try decoder.decode(T.self, from: data)
+                completion(.success(decodedData))
             } catch {
                 completion(.failure(.parseError))
             }
         }
         task.resume()
     }
+
+    // JSONDecoding 분리
 }
