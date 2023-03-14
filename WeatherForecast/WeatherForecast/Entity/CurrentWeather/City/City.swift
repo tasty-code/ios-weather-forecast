730356af3031ba8e13830a87318e7c1ad73b527a
Jason <ggj00125@gmail.com>
2023-03-14 16:35:48 +0900

    feat: City 타입 구현

---
+++ WeatherForecast/WeatherForecast/Entity/CurrentWeather/City/City.swift
@@ -0,0 +1,22 @@
+//
+//  City.swift
+//  WeatherForecast
+//
+//  Created by Jason on 2023/03/14.
+//
+
+import Foundation
+
+struct City {
+    let id: Double
+    let name: String
+    let findname: String
+    let coordinate: Coordinate
+    let zoom: Int
+    let language: [Language]
+    
+    private enum CodingKeys: String, CodingKey {
+        case coordinate = "coord"
+        case language = "langs"
+    }
+}
