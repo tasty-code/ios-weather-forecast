45adce2a32048baf9dcf17d7d869eb7dcd458191
Jason <ggj00125@gmail.com>
2023-03-14 16:35:27 +0900

    feat: Coordinate 타입 구현

---
+++ WeatherForecast/WeatherForecast/Entity/CurrentWeather/City/Coordinate.swift
@@ -0,0 +1,18 @@
+//
+//  Coordinate.swift
+//  WeatherForecast
+//
+//  Created by Jason on 2023/03/14.
+//
+
+import Foundation
+
+struct Coordinate {
+    let longitude: Double
+    let latitude: Double
+    
+    private enum CodingKeys: String, CodingKey {
+        case longitude = "lon"
+        case latitude = "lat"
+    }
+}
