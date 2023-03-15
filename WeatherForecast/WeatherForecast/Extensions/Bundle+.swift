428a3709144218ef29060831bc5efdc69bbc6325
Mason Kim <qwerty3345@naver.com>
2023-03-15 16:51:25 +0900

    refactor: API Key 숨기기 .xcconfig 방식으로 변경

--- WeatherForecast/WeatherForecast/Extensions/Bundle+.swift
+++ WeatherForecast/WeatherForecast/Extensions/Bundle+.swift
@@ -9,10 +9,9 @@ import Foundation
 
 extension Bundle {
     var apiKey: String {
-        guard let file = path(forResource: "WeatherAPIKey", ofType: "plist"),
-              let resource = NSDictionary(contentsOfFile: file),
-              let key = resource["API_KEY"] as? String else { return "" }
-
+        guard let key = object(forInfoDictionaryKey: "OpenWeatherAPIKey") as? String else {
+            return ""
+        }
         return key
     }
 }
