521a09555bbafd9bafa7b00fea992a7db0655281
Lust3r <s1skyblue1s@gmail.com>
2023-03-15 16:39:57 +0900

    feat: API 데이터를 받을 Model 생성 및 API 호출 메서드 구현

--- WeatherForecast/WeatherForecast/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -8,11 +8,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var model: Model?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        callAPI()
     }
 
+    private func callAPI() {
+        guard let weatherBaseURL = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=37.533624&lon=126.963206&appid=") else { return }
+        let weatherUrlRequest = URLRequest(url: weatherBaseURL)
+        let decoder = JSONDecoder()
+
+        let task = URLSession.shared.dataTask(with: weatherUrlRequest) { data, response, error in
+            if let data = data {
+                do {
+                    try self.model = decoder.decode(Model.self, from: data)
+                    print(self.model)
+                } catch {
+                    print(error)
+                }
+            }
+        }
+        task.resume()
+    }
 
 }
 
