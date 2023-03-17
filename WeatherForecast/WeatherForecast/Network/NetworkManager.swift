df082cbc8c3c182e68104be8cc7a6bc7ef86b799
SunnnySong <tjsghktjswls@naver.com>
2023-03-17 11:27:19 +0900

    refactor: NetworkManager task 생성 및 수정

--- WeatherForecast/WeatherForecast/Network/NetworkManager.swift
+++ WeatherForecast/WeatherForecast/Network/NetworkManager.swift
@@ -7,15 +7,29 @@
 
 import UIKit
 
-class NetworkManager {
-    let session: URLSession
+final class NetworkManager {
+    private let session: URLSession
+    private let networkModel: NetworkModel
     
     init(session: URLSession = .shared) {
         self.session = session
+        self.networkModel = NetworkModel()
     }
     
     func fetchWeatherInformation(of weatherAPI: WeatherAPI, in coordinate: Coordinate) {
         let url = weatherAPI.makeWeatherURL(coordinate: coordinate)
         let urlRequest = URLRequest(url: url)
+        
+        let task = networkModel.task(session: session, urlRequest: urlRequest) { result in
+            
+            switch result {
+            case .success(let data):
+                let decodedData = self.networkModel.decode(from: data, to: weatherAPI.decodingType)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+        
+        task.resume()
     }
 }
