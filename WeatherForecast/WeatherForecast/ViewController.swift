0e973b3b99d82c13353c0493a2aa19549f1ec6db
iyeahh <yboravender@gmail.com>
2023-03-21 16:23:17 +0900

    feat: 현재 위치의 위도와 경도로 날씨 API 호출

--- WeatherForecast/WeatherForecast/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -5,18 +5,39 @@
 // 
 
 import UIKit
+import CoreLocation
 
-final class ViewController: UIViewController {
+final class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        APIService.shared.fetchFiveDayWeather(lat: 37.53, lon: 126.96) { result in
-                    switch result {
-                    case .success(let currentWeather):
-                        print(currentWeather)
-                    case .failure(let error):
-                        print(error.errorDescription as Any)
-                    }
-                }
+
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let currentLocation = locations.last else { return }
+
+        let latitude = currentLocation.coordinate.latitude
+        let longitude = currentLocation.coordinate.longitude
+
+        APIService.shared.fetchCurrentWeather(lat: latitude, lon: longitude) {
+            result in
+            switch result {
+            case .success(let currentWeather):
+                print(currentWeather)
+            case .failure(let error):
+                print(error.errorDescription as Any)
+            }
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location update failed with error: \(error.localizedDescription)")
+    }
+
 }
