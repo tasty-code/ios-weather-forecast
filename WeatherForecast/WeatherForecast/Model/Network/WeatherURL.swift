7d2b0ef75da4e0113e13dd9ba11ce0529117b8ee
bomyuniverse <71758542+calledBlu@users.noreply.github.com>
2023-03-21 14:48:50 +0900

    [fix] Unit Test를 통해 URL 생성 시 발견되던 문제점 수정
    - 현재 날씨와 5일 예보를 동시에 호출하는 경우 path가 변경되지 않고 추가되던 문제 발생
    - URLComponents 인스턴스를 make 메서드 내부에서 생성하도록 수정
    - 휴먼에러 수정: language -> lang

--- WeatherForecast/WeatherForecast/Model/Network/WeatherURL.swift
+++ WeatherForecast/WeatherForecast/Model/Network/WeatherURL.swift
@@ -8,21 +8,22 @@
 import Foundation
 
 enum WeatherURL {
-    private static var baseURL = URLComponents(string: "https://api.openweathermap.org/data/2.5/")
+    private static let baseURL = "https://api.openweathermap.org/data/2.5/"
     private static let measurementUnit = "metric"
     private static let language = "kr"
 
     static func make(at coordinate: CurrentCoordinate, weatherRange: WeatherRange) throws -> URL {
-        baseURL?.path.append(weatherRange.description)
-        baseURL?.queryItems = [
+        var components = URLComponents(string: baseURL)
+        components?.path.append(weatherRange.description)
+        components?.queryItems = [
             URLQueryItem(name: "lat", value: "\(coordinate.latitude)"),
             URLQueryItem(name: "lon", value: "\(coordinate.longitude)"),
             URLQueryItem(name: "units", value: measurementUnit),
-            URLQueryItem(name: "language", value: language),
+            URLQueryItem(name: "lang", value: language),
             URLQueryItem(name: "appid", value: Bundle.main.apiKey)
         ]
         
-        guard let url = baseURL?.url else { throw WeatherNetworkError.invalidURL }
+        guard let url = components?.url else { throw WeatherNetworkError.invalidURL }
         
         return url
     }
