42088497b057f3be5a3e840e9e0d7dea78d05ddb
ueunli <cdenen20@gmail.com>
2023-03-21 15:49:33 +0900

    [feat] 현재 위치를 가져오는 기능 구현
    - CoreLocation을 import
    - ViewController가 CLLocationManager 인스턴스 locationManager를 갖도록 선언
    - ViewController가 CLLocationManagerDelegate를 채택 및 locationManager를 사용하기 위한 delegate method 구현
    - Info.plist에 when-in-use 위치정보 사용 방법에 대한 설명을 제공하기 위해 key설정

--- WeatherForecast/WeatherForecast/Controller/ViewController.swift
+++ WeatherForecast/WeatherForecast/Controller/ViewController.swift
@@ -5,12 +5,32 @@
 // 
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    private let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways:
+            manager.requestLocation()
+        default:
+            manager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locationManager.location!.coordinate
+        let coordinate = CurrentCoordinate(longitude: location.longitude, latitude: location.latitude)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
     }
 }
 
