f9da1b5bba3c8b7d863402badf35f6291810fafc
ueunli <cdenen20@gmail.com>
2023-03-21 14:24:31 +0900

    [refactor] URLComponents를 이용해 url을 생성하도록 수정
    - URL을 보다 구조적인 측면에서 생성 및 활용, 문자열 연산을 줄이기 위해 기본제공타입인 URLComponents 인스턴스를 사용하도록 함
    - queryItems 및 path 속성에 매개변수들을 할당한 뒤 완성된 url을 반환하도록 함
    - query 요소들을 key-value형식으로 이루어진 URLQueryItem으로서 queryItems에 할당
    - 불러오는 api종류를 결정할 WeatherRange를 path에 append

--- WeatherForecast/WeatherForecast/Model/Network/WeatherURL.swift
+++ WeatherForecast/WeatherForecast/Model/Network/WeatherURL.swift
@@ -8,23 +8,22 @@
 import Foundation
 
 enum WeatherURL {
-    private static let baseURL = "https://api.openweathermap.org/data/2.5/"
+    private static var baseURL = URLComponents(string: "https://api.openweathermap.org/data/2.5/")
     private static let measurementUnit = "metric"
     private static let language = "kr"
-    private static let apiKey = Bundle.main.apiKey
 
     static func make(at coordinate: CurrentCoordinate, weatherRange: WeatherRange) throws -> URL {
-        let query = ["lat=\(coordinate.latitude)",
-                     "lon=\(coordinate.longitude)",
-                     "units=\(measurementUnit)",
-                     "lang=\(language)",
-                     "appid=\(apiKey)"].joined(separator: "&")
-        let urlString = baseURL + weatherRange.description + "?" + query
-
-        guard let url = URL(string: urlString) else {
-            throw WeatherNetworkError.invalidURL
-        }
-
+        baseURL?.path.append(weatherRange.description)
+        baseURL?.queryItems = [
+            URLQueryItem(name: "lat", value: "\(coordinate.latitude)"),
+            URLQueryItem(name: "lon", value: "\(coordinate.longitude)"),
+            URLQueryItem(name: "units", value: measurementUnit),
+            URLQueryItem(name: "language", value: language),
+            URLQueryItem(name: "appid", value: Bundle.main.apiKey)
+        ]
+        
+        guard let url = baseURL?.url else { throw WeatherNetworkError.invalidURL }
+        
         return url
     }
 
