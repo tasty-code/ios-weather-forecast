bdccdd7e8071e72a363b9f0433f0826150733ed5
Jason <ggj00125@gmail.com>
2023-03-23 16:24:57 +0900

    feat: location 연산 프로퍼티 및 authorize 메서드 구현

--- WeatherForecast/WeatherForecast/Domain/UserLocation.swift
+++ WeatherForecast/WeatherForecast/Domain/UserLocation.swift
@@ -8,11 +8,21 @@
 import CoreLocation
 
 final class UserLocation: NSObject, CLLocationManagerDelegate {
+    
     static let shard = UserLocation()
+    
+    var location: CLLocation? {
+        sharedLocationManager.location
+    }
 
     lazy var sharedLocationManager: CLLocationManager = {
         let newLocationmanager = CLLocationManager()
         newLocationmanager.delegate = self
         return newLocationmanager
     }()
+    
+    //MARK: - Authorization
+    func authorize() {
+        sharedLocationManager.requestWhenInUseAuthorization()
+    }
 }
