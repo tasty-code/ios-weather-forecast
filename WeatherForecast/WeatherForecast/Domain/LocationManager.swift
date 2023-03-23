6a726059b33c504569fcfb79de22fc4220139e3e
Jason <ggj00125@gmail.com>
2023-03-23 16:02:07 +0900

    feat: LocationManager 파일 생성 및 클래스 타입으로 정의

---
+++ WeatherForecast/WeatherForecast/Domain/LocationManager.swift
@@ -0,0 +1,13 @@
+//
+//  LocationManager.swift
+//  WeatherForecast
+//
+//  Created by Jason on 2023/03/23.
+//
+
+import Foundation
+import CoreLocation
+
+final class LocationManager: CLLocationManager {
+    
+}
