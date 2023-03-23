1b57fb0575f2d24e391739798b5eaf982bfc74a4
Jason <ggj00125@gmail.com>
2023-03-23 16:55:24 +0900

    feat: address 메서드 구현

--- WeatherForecast/WeatherForecast/Domain/UserLocation.swift
+++ WeatherForecast/WeatherForecast/Domain/UserLocation.swift
@@ -9,12 +9,13 @@ import CoreLocation
 
 final class UserLocation: NSObject, CLLocationManagerDelegate {
     
+    //MARK: - Property
     static let shard = UserLocation()
     
     var location: CLLocation? {
         sharedLocationManager.location
     }
-
+    
     lazy var sharedLocationManager: CLLocationManager = {
         let newLocationmanager = CLLocationManager()
         newLocationmanager.delegate = self
@@ -24,5 +25,21 @@ final class UserLocation: NSObject, CLLocationManagerDelegate {
     //MARK: - Authorization
     func authorize() {
         sharedLocationManager.requestWhenInUseAuthorization()
+    }
+    
+    func address() {
+        guard let newLocation = location else {
+            return
+        }
+        
+        let geocoder = CLGeocoder()
+        
+        geocoder.reverseGeocodeLocation(newLocation) { placemark, error in
+            guard error == nil else { return }
+            
+            if let placemark = placemark?.first {
+                print(placemark)
+            }
+        }
     }
 }
