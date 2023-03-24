e5f5e6c2c97d05b254ba56321191a26b1eee6456
Lust3r <s1skyblue1s@gmail.com>
2023-03-24 16:17:11 +0900

    chore: 사용하지 않은 NetworkError case 제거

--- WeatherForecast/WeatherForecast/Network/NetworkError.swift
+++ WeatherForecast/WeatherForecast/Network/NetworkError.swift
@@ -12,12 +12,6 @@ enum NetworkError: LocalizedError {
     case apiFilePath
     case apiResource
     case apiKey
-    case unknownError
-    case invalidHttpStatusCode(Int)
-    case components
-    case urlRequest(Error)
-    case parsing(Error)
-    case emptyData
     case decodeError
     
     var errorDescription: String? {
@@ -26,12 +20,6 @@ enum NetworkError: LocalizedError {
         case .apiKey: return "api key 에러"
         case .apiResource: return "api resource 에러"
         case .apiFilePath: return "api file path 에러"
-        case .unknownError: return "알수 없는 에러입니다."
-        case .invalidHttpStatusCode: return "status코드가 200~299가 아닙니다."
-        case .components: return "components를 생성 에러가 발생했습니다."
-        case .urlRequest: return "URL request 관련 에러가 발생했습니다."
-        case .parsing: return "데이터 parsing 중에 에러가 발생했습니다."
-        case .emptyData: return "data가 비어있습니다."
         case .decodeError: return "decode 에러가 발생했습니다."
         }
     }
