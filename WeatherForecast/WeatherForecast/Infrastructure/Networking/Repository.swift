ba8164c83459e6604cfd03d291ac98bf068f6fc2
ohdair <ohdiar@gmail.com>
2023-03-24 19:20:07 +0900

    refactor: loadData 메서드 삭제

--- WeatherForecast/WeatherForecast/Infrastructure/Networking/Repository.swift
+++ WeatherForecast/WeatherForecast/Infrastructure/Networking/Repository.swift
@@ -16,35 +16,7 @@ class Repository {
     typealias Handler = (Result<WeatherModel, NetworkEntityLoadingError>) -> Void
     
     let session = URLSession.shared
-    
-    func loadData(location: CLLocationCoordinate2D, path: URLPath) throws {
-        let url = try URLPath.configureURL(of: path, with: location)
-        
-        session.dataTask(with: url) { data, response, error in
-            guard error == nil else {
-                if (error as? URLError)?.code == .timedOut {
-                    print(error?.localizedDescription ?? error.debugDescription)
-                }
-                return
-            }
-            
-            guard let httpResponse = response as? HTTPURLResponse,
-                  (200...299).contains(httpResponse.statusCode) else {
-                print((response as? HTTPURLResponse)?.statusCode ?? 404)
-                return
-            }
-            
-            guard let data = data else {
-                return
-            }
-            
-            guard let wishData = self.loadJSON(weatherType: path.weatherMetaType, weatherData: data) else {
-                return
-            }
-            print(wishData)
-        }.resume()
-    }
-    
+
     //MARK: - URL Result Test
     func loadWeatherEntity(with location: CLLocationCoordinate2D, path: URLPath, then completion: @escaping Handler) throws {
         let url = try URLPath.configureURL(of: path, with: location)
@@ -82,8 +54,9 @@ extension URLSession {
         dataTask(with: url) { data, response, error in
             if let error = error {
                 completion(.failure(error))
-            } else {
-                completion(.success(data ?? Data()))
+            }
+            if let data = data {
+                completion(.success(data))
             }
         }
     }
