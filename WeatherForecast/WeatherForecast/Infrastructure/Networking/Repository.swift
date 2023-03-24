1e05c4602739a47a9aa98876b2f55f2cb895436f
Jason <ggj00125@gmail.com>
2023-03-24 17:26:02 +0900

    Refactor: URLSession Result 타입 적용으로 리펙터링

--- WeatherForecast/WeatherForecast/Infrastructure/Networking/Repository.swift
+++ WeatherForecast/WeatherForecast/Infrastructure/Networking/Repository.swift
@@ -7,10 +7,18 @@
 
 import CoreLocation
 
+enum NetworkEntityLoadingError: Error {
+    case networkFailure(Error)
+    case invalidData
+}
+
 class Repository {
+    typealias Handler = (Result<WeatherModel, NetworkEntityLoadingError>) -> Void
+    
+    let session = URLSession.shared
+    
     func loadData(location: CLLocationCoordinate2D, path: URLPath) throws {
         let url = try URLPath.configureURL(of: path, with: location)
-        let session = URLSession.shared
         
         session.dataTask(with: url) { data, response, error in
             guard error == nil else {
@@ -37,6 +45,25 @@ class Repository {
         }.resume()
     }
     
+    //MARK: - URL Result Test
+    func loadWeatherEntity(with location: CLLocationCoordinate2D, path: URLPath, then completion: @escaping Handler) throws {
+        let url = try URLPath.configureURL(of: path, with: location)
+        
+        let task = session.dataTask(with: url) { [self] result in
+            switch result {
+            case .success(let data):
+                if let wishData = loadJSON(weatherType: path.weatherMetaType, weatherData: data) {
+                    completion(.success(wishData))
+                } else {
+                    completion(.failure(.invalidData))
+                }
+            case .failure(let error):
+                completion(.failure(.networkFailure(error)))
+            }
+        }
+        task.resume()
+    }
+    
     private func loadJSON(weatherType: WeatherModel.Type, weatherData: Data) -> WeatherModel? {
         let decoder = JSONDecoder()
         
@@ -46,6 +73,18 @@ class Repository {
         } catch {
             print("Unable to decode \(weatherData): (error)")
             return nil
+        }
+    }
+}
+
+extension URLSession {
+    func dataTask(with url: URL, completion: @escaping (Result<Data, Error>) -> Void) -> URLSessionDataTask {
+        dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(data ?? Data()))
+            }
         }
     }
 }
