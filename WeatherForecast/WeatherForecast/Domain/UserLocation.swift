2c171f058e4dba2a2c23c2c7be456c3f0a986ee1
ohdair <ohdiar@gmail.com>
2023-03-24 14:07:41 +0900

    feat: 위도/경도를 기반으로 현재 위치 출력하는 메서드 추가
    - OO구 출력을 하기 위해 addressDictionary 사용
    - 우편번호가 없어서 발생하는 에러를 위해 UserLocationError 생성

--- WeatherForecast/WeatherForecast/Domain/UserLocation.swift
+++ WeatherForecast/WeatherForecast/Domain/UserLocation.swift
@@ -7,6 +7,10 @@
 
 import CoreLocation
 
+enum UserLocationError: Error {
+    case withoutZipCode
+}
+
 final class UserLocation: NSObject, CLLocationManagerDelegate {
     
     //MARK: - Property
@@ -15,31 +19,44 @@ final class UserLocation: NSObject, CLLocationManagerDelegate {
     var location: CLLocation? {
         sharedLocationManager.location
     }
-    
+
     lazy var sharedLocationManager: CLLocationManager = {
         let newLocationmanager = CLLocationManager()
         newLocationmanager.delegate = self
         return newLocationmanager
     }()
-    
+
     //MARK: - Authorization
     func authorize() {
         sharedLocationManager.requestWhenInUseAuthorization()
     }
-    
-    func address() {
+
+    func address(completion: @escaping (Result<String, Error>) -> Void) {
         guard let newLocation = location else {
+            completion(.failure(UserLocationError.withoutZipCode))
             return
         }
-        
+
         let geocoder = CLGeocoder()
-        
-        geocoder.reverseGeocodeLocation(newLocation) { placemark, error in
+
+        geocoder.reverseGeocodeLocation(newLocation) { (placemark, error) in
             guard error == nil else { return }
-            
-            if let placemark = placemark?.first {
-                print(placemark)
+            if let placemark = placemark?.first,
+               let address = placemark.formattedAddress {
+                completion(.success(address))
             }
         }
+    }
+}
+
+private extension CLPlacemark {
+    var formattedAddress: String? {
+        guard let infomation = self.addressDictionary?["FormattedAddressLines"] as? [String],
+              let value = infomation.first else {
+            // 우편번호가 없는 경우
+            return nil
+        }
+
+        return value.split(separator: " ")[1...2].joined(separator: " ")
     }
 }
--- WeatherForecast/WeatherForecast/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -16,12 +16,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         UserLocation.shard.authorize()
-
-        do {
-            try repository.loadData(location: location, path: .currentWeather)
-            try repository.loadData(location: location, path: .forecastWeather)
-        } catch {
-            print(error.localizedDescription)
+        //MARK: - 테스트용 출력
+        UserLocation.shard.address { (result) in
+            switch result {
+            case .success(let address):
+                print(address)
+            case .failure(let error):
+                print(error)
+            }
         }
+
+        //MARK: - API 호출을 잠시 감췄습니다
+//        do {
+//            try repository.loadData(location: location, path: .currentWeather)
+//            try repository.loadData(location: location, path: .forecastWeather)
+//        } catch {
+//            print(error.localizedDescription)
+//        }
     }
 }
