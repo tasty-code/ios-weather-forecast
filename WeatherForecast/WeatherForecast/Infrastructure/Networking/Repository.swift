c94e3547c60b4ac5c4e95cd9a485c4e4c6489fd4
Jason <ggj00125@gmail.com>
2023-03-28 16:59:11 +0900

    refactor: url throw 해결을 위해 do-catch 구문 사용

--- WeatherForecast/WeatherForecast/Infrastructure/Networking/Repository.swift
+++ WeatherForecast/WeatherForecast/Infrastructure/Networking/Repository.swift
@@ -12,36 +12,41 @@ class Repository {
     
     private let session = URLSession.shared
     
-    func loadData(with location: CLLocationCoordinate2D, path: URLPath, completion: @escaping (Error) -> Void) throws {
-        let url = try URLPath.configureURL(of: path, with: location)
-        var urlRequest = URLRequest(url: url)
+    func loadData(with location: CLLocationCoordinate2D, path: URLPath, completion: @escaping (WeatherModel?, Error?) -> Void) {
         
-        urlRequest.httpMethod = "GET"
-        session.dataTask(with: urlRequest) { data, response, error in
-            guard error == nil else {
-                completion(NetworkError.notConnected)
-                return
-            }
+        do {
+            let url = try URLPath.configureURL(of: path, with: location)
+            var urlRequest = URLRequest(url: url)
             
-            guard let httpResponse = response as? HTTPURLResponse,
-                  (200...299).contains(httpResponse.statusCode) else {
-                let responseError = HTTPResponseError.error(statusCode: (response as? HTTPURLResponse)?.statusCode ?? 404,
-                                                            description: response.debugDescription)
-                completion(responseError)
-                return
-            }
-            
-            guard let data = data else {
-                completion(NetworkEntityLoadingError.networkFailure)
-                return
-            }
-            
-            guard let wishData = self.loadJSON(weatherType: path.weatherMetaType, weatherData: data) else {
-                completion(NetworkEntityLoadingError.invalidData)
-                return
-            }
-            print(wishData)
-        }.resume()
+            urlRequest.httpMethod = "GET"
+            session.dataTask(with: urlRequest) { data, response, error in
+                guard error == nil else {
+                    completion(nil, NetworkError.notConnected)
+                    return
+                }
+                
+                guard let httpResponse = response as? HTTPURLResponse,
+                      (200...299).contains(httpResponse.statusCode) else {
+                    let responseError = HTTPResponseError.error(statusCode: (response as? HTTPURLResponse)?.statusCode ?? 404,
+                                                                description: response.debugDescription)
+                    completion(nil, responseError)
+                    return
+                }
+                
+                guard let data = data else {
+                    completion(nil, NetworkEntityLoadingError.networkFailure)
+                    return
+                }
+                
+                guard let wishData = self.loadJSON(weatherType: path.weatherMetaType, weatherData: data) else {
+                    completion(nil, NetworkEntityLoadingError.invalidData)
+                    return
+                }
+                completion(wishData, nil)
+            }.resume()
+        } catch {
+            completion(nil, error)
+        }
     }
     
     private func loadJSON(weatherType: WeatherModel.Type, weatherData: Data) -> WeatherModel? {
