247ac13adf6d8c308f69a560fa4b6a65a79882c1
Mason Kim <qwerty3345@naver.com>
2023-03-30 16:17:25 +0900

    refactor: 메인 ViewController 이름 변경

--- WeatherForecast/WeatherForecast/Application/AppDelegate.swift
+++ WeatherForecast/WeatherForecast/Application/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        window?.rootViewController = ViewController()
+        window?.rootViewController = WeatherListViewController()
 
         return true
     }
