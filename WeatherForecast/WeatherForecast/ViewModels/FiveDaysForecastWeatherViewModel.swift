e9c77c3ca4e23b120088373605c3ecf38b306161
longlivedrgn <kimye9808@naver.com>
2023-03-31 19:01:13 +0900

    feat: FiveDaysForecastWeatherViewModel 속 makeForecastWeather, makeForecastImage 메소드 생성

--- WeatherForecast/WeatherForecast/ViewModels/FiveDaysForecastWeatherViewModel.swift
+++ WeatherForecast/WeatherForecast/ViewModels/FiveDaysForecastWeatherViewModel.swift
@@ -16,4 +16,29 @@ final class FiveDaysForecastWeatherViewModel {
             let date: String
             let temperature: Double
         }
+    
+    func makeForecastWeather(weatherAPIManager: WeatherAPIManager?,
+                                 coordinate: Coordinate,
+                                 location: CLLocation,
+                                 completion: @escaping (String, Day) -> Void
+        ) {
+            weatherAPIManager?.fetchWeatherInformation(of: .fiveDaysForecast, in: coordinate) { data in
+                guard let forecastData = data as? FiveDaysForecastDTO else { return }
+                for eachData in forecastData.list {
+                    guard let icon = eachData.weather.first?.icon else { return }
+                    completion(icon, eachData)
+                }
+            }
+        }
+        
+        func makeForecastImage(weatherAPIManager: WeatherAPIManager?,
+                               icon: String,
+                               eachData: Day
+        ) {
+            weatherAPIManager?.fetchWeatherImage(icon: icon) { image in
+                let fiveDaysForecast = FiveDaysForecast(image: image, date: eachData.time, temperature: eachData.temperature.temperature)
+                print(fiveDaysForecast)
+
+            }
+        }
 }
