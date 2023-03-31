09070d30fce739af399e74ce431489f083c5f485
Sunnnysong <tjsghktjswls@naver.com>
2023-03-31 19:05:18 +0900

    feat: WeatherViewModel 생성 및 LocationDelegate 연결

--- WeatherForecast/WeatherForecast/ViewModels/WeatherViewModel.swift
+++ WeatherForecast/WeatherForecast/ViewModels/WeatherViewModel.swift
@@ -10,9 +10,75 @@ import CoreLocation
 
 final class WeatherViewModel {
     
+    private let fiveDaysForecastWeatherViewModel = FiveDaysForecastWeatherViewModel()
+    private let currentWeatherViewModel = CurrentWeatherViewModel()
+    
+    private let locationManager = CoreLocationManager()
     private let weatherAPIManager: WeatherAPIManager?
+    
+    var fiveDaysForecastWeather: [FiveDaysForecastWeatherViewModel.FiveDaysForecast] = []
+    var currentWeather: CurrentWeatherViewModel.CurrentWeather?
     
     init(networkModel: NetworkModel = NetworkModel(session: URLSession.shared)) {
         weatherAPIManager = WeatherAPIManager(networkModel: networkModel)
+        
+        locationManager.locationDelegate = self
+    }
+    
+    func makeCoordinate(from location: CLLocation) -> Coordinate {
+        
+        let latitude = location.coordinate.latitude
+        let longitude = location.coordinate.longitude
+        
+        return Coordinate(longitude: longitude, latitude: latitude)
+    }
+    
+    func makeWeatherData(locationManager: CoreLocationManager, weatherAPIManager: WeatherAPIManager?) {
+        
+        guard let location = locationManager.locationManager?.location else { return }
+        let coordinate = self.makeCoordinate(from: location)
+        
+        currentWeatherViewModel.makeCurrentAddress(
+            locationManager: locationManager,
+            location: location
+        ) { [weak self] address in
+            
+            self?.currentWeatherViewModel.makeCurrentInformation(
+                weatherAPIManager: weatherAPIManager,
+                coordinate: coordinate,
+                location: location,
+                address: address
+            ) { [weak self] iconString, weatherData in
+                
+                self?.currentWeatherViewModel.makeCurrentImage(
+                    weatherAPIManager: weatherAPIManager,
+                    iconString: iconString,
+                    address: address,
+                    weatherData: weatherData
+                )
+            }
+        }
+        
+        self.fiveDaysForecastWeatherViewModel.makeForecastWeather(
+            weatherAPIManager: weatherAPIManager,
+            coordinate: coordinate,
+            location: location
+        ) { [weak self] iconString, eachData in
+            
+            self?.fiveDaysForecastWeatherViewModel.makeForecastImage(
+                weatherAPIManager: weatherAPIManager,
+                icon: iconString,
+                eachData: eachData
+            )
+        }
+    }
+}
+
+extension WeatherViewModel: LocationDelegate {
+    func didUpdateLocation() {
+        makeWeatherData(
+            locationManager: locationManager,
+            weatherAPIManager: weatherAPIManager
+        )
     }
 }
