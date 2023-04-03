7b575808f8b1d529101e4df42f1b9acc18816c6b
longlivedrgn <kimye9808@naver.com>
2023-04-03 13:38:25 +0900

    refactor: APIKeys.swift .gitignore에서 제거

---
+++ WeatherForecast/WeatherForecast/Network/API/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  WeatherForecast
+//
+//  Created by 김용재 on 2023/03/16.
+//
+
+import UIKit
+
+enum APIKeys {
+    
+    static let openWeatherKey = "856318770047c050ca52aa6c694f9064"
+}
