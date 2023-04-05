100372a846a35eb7d49a27af698c304385657218
Lust3r <s1skyblue1s@gmail.com>
2023-04-05 17:16:55 +0900

    feat: CollectionView 기본 형식 구현

--- WeatherForecast/WeatherForecast/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -11,14 +11,40 @@ class ViewController: UIViewController {
     let networkManager = NetworkManager()
     let locationManager = LocationManager()
 
+    var collectionView: UICollectionView = {
+        var layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+
+        var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+
+        return collectionView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.delegate = self
+
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        view.addSubview(collectionView)
+        collectionView.register(WeatherCollectionViewCell.self, forCellWithReuseIdentifier: WeatherCollectionViewCell.cellIdentifier)
+        configureCollectionViewRestraint()
         setUp()
     }
     
     private func setUp() {
         locationManager.startUpdatingLocation()
+    }
+
+    private func configureCollectionViewRestraint() {
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 }
 
@@ -26,5 +52,57 @@ extension ViewController: LocationManagerDelegate {
     func locationManager(_ manager: LocationManager, didUpdateLocation location: CLLocation) {
         networkManager.updateLocation(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
         networkManager.callAPI()
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WeatherCollectionViewCell.cellIdentifier, for: indexPath) as? WeatherCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+
+        cell.indexpathLabel.text = "\(indexPath.section + 1)"
+
+        return cell
+    }
+}
+
+class WeatherCollectionViewCell: UICollectionViewCell {
+    static let cellIdentifier = "WeatherCollectionViewCell"
+
+    var indexpathLabel = UILabel()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        addSubview(indexpathLabel)
+        configureLabelConstraint()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    func configureLabelConstraint() {
+        indexpathLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            indexpathLabel.topAnchor.constraint(equalTo: self.topAnchor),
+            indexpathLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+            indexpathLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            indexpathLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
     }
 }
