2e3bccf0d9629965ceea47abc0fedb1f0127e8aa
longlivedrgn <kimye9808@naver.com>
2023-04-05 11:16:43 +0900

    feat: collectionview 생성 및 layout list configuration 설정하기

--- WeatherForecast/WeatherForecast/Views/WeatherViewController.swift
+++ WeatherForecast/WeatherForecast/Views/WeatherViewController.swift
@@ -10,9 +10,24 @@ import CoreLocation
 class WeatherViewController: UIViewController {
     
     private var weatherViewModel = WeatherViewModel()
+    private var weatherCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureHierarchy()
+    }
+}
+
+extension WeatherViewController {
+    private func configureHierarchy() {
+        weatherCollectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+    }
+    
+    private func createLayout() -> UICollectionViewLayout {
+        var configuration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        configuration.headerMode = .supplementary
+        let layout = UICollectionViewCompositionalLayout.list(using: configuration)
+        return layout
     }
 }
 
