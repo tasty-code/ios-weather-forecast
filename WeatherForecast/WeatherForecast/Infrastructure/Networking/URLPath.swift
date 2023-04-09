cd25ae162947ba46959a3135adb5bb1ea57209bc
Jason <ggj00125@gmail.com>
2023-04-09 14:14:14 +0900

    refactor: let -> var로 변경

--- WeatherForecast/WeatherForecast/Infrastructure/Networking/URLPath.swift
+++ WeatherForecast/WeatherForecast/Infrastructure/Networking/URLPath.swift
@@ -11,7 +11,7 @@ enum URLPath: String, CaseIterable {
     case currentWeather
     case forecastWeather
     
-    static let iconList = ["01d", "01n", "02d", "02n", "03d", "03n","04d", "04n",
+    static var iconList = ["01d", "01n", "02d", "02n", "03d", "03n","04d", "04n",
                            "09d", "09n", "10d", "10n", "11d", "11n", "13d", "13n",
                            "50d", "50n",]
     
