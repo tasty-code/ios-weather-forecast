62bfb450e70e2f52c9054e1c39fd0fea43b38a4f
longlivedrgn <kimye9808@naver.com>
2023-04-12 09:55:24 +0900

    fix: 들여쓰기 수정

--- WeatherForecast/WeatherForecast/Views/Cells/CurrentWeatherCell.swift
+++ WeatherForecast/WeatherForecast/Views/Cells/CurrentWeatherCell.swift
@@ -10,44 +10,44 @@ import UIKit
 final class CurrentWeatherCell: UICollectionViewListCell {
     
     var currentWeather: CurrentWeatherViewModel.CurrentWeather?
-       
-       override func updateConfiguration(using state: UICellConfigurationState) {
-           super.updateConfiguration(using: state)
-           
-           var configuration = UIListContentConfiguration.subtitleCell()
-           
-           guard let address = currentWeather?.address,
-                 let minimumTemperature = currentWeather?.temperatures.minimumTemperature,
-                 let maximumTemperature = currentWeather?.temperatures.maximumTemperature,
-                 let currentTemperature = currentWeather?.temperatures.averageTemperature
-           else { return }
-
-           let addressAndTemperatureText: String = """
+    
+    override func updateConfiguration(using state: UICellConfigurationState) {
+        super.updateConfiguration(using: state)
+        
+        var configuration = UIListContentConfiguration.subtitleCell()
+        
+        guard let address = currentWeather?.address,
+              let minimumTemperature = currentWeather?.temperatures.minimumTemperature,
+              let maximumTemperature = currentWeather?.temperatures.maximumTemperature,
+              let currentTemperature = currentWeather?.temperatures.averageTemperature
+        else { return }
+        
+        let addressAndTemperatureText: String = """
            \(address)
            최저 \(minimumTemperature.changeWeatherFormat().degree) 최소 \(maximumTemperature.changeWeatherFormat().degree)
            """
-           
-           let paragraphStyle = NSMutableParagraphStyle()
-           paragraphStyle.lineSpacing = 6
-           
-           let addressAndTemperatureTextAttributes: [NSAttributedString.Key: Any] = [
-               .font : UIFont.systemFont(ofSize: 13),
-               .paragraphStyle : paragraphStyle,
-           ]
-           
-           configuration.attributedText = NSAttributedString(string: addressAndTemperatureText, attributes: addressAndTemperatureTextAttributes)
-           configuration.textProperties.color = .white
-           
-           let currentTemperatureTextAttribtues: [NSAttributedString.Key: Any] = [
-               .font : UIFont.systemFont(ofSize: 30, weight: .semibold)
-           ]
-           
-           configuration.secondaryAttributedText = NSAttributedString(string: currentTemperature.changeWeatherFormat().degree, attributes: currentTemperatureTextAttribtues)
-           configuration.secondaryTextProperties.color = .white
-           configuration.textToSecondaryTextVerticalPadding = 10
-           
-           configuration.image = currentWeather?.image
-           
-           contentConfiguration = configuration
-       }
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 6
+        
+        let addressAndTemperatureTextAttributes: [NSAttributedString.Key: Any] = [
+            .font : UIFont.systemFont(ofSize: 13),
+            .paragraphStyle : paragraphStyle,
+        ]
+        
+        configuration.attributedText = NSAttributedString(string: addressAndTemperatureText, attributes: addressAndTemperatureTextAttributes)
+        configuration.textProperties.color = .white
+        
+        let currentTemperatureTextAttribtues: [NSAttributedString.Key: Any] = [
+            .font : UIFont.systemFont(ofSize: 30, weight: .semibold)
+        ]
+        
+        configuration.secondaryAttributedText = NSAttributedString(string: currentTemperature.changeWeatherFormat().degree, attributes: currentTemperatureTextAttribtues)
+        configuration.secondaryTextProperties.color = .white
+        configuration.textToSecondaryTextVerticalPadding = 10
+        
+        configuration.image = currentWeather?.image
+        
+        contentConfiguration = configuration
+    }
 }
