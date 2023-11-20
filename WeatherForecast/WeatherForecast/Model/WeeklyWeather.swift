1b301c0a4818674b6a55808783177bb734174e13
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2023-11-20 16:55:05 +0900

    [Feat] open weather API 5일 일기 예보 DTO 구현

---
+++ WeatherForecast/WeatherForecast/Model/WeeklyWeather.swift
@@ -0,0 +1,56 @@
+//
+//  WeeklyWeather.swift
+//  WeatherForecast
+//
+//  Created by 김수경 on 2023/11/20.
+//
+
+import Foundation
+
+struct WeeklyWeather: Decodable {
+    var cod: String
+    var message: Int
+    var cnt: Int
+    var list: [List]
+    var city: City
+}
+
+// MARK: - List
+
+struct List: Decodable {
+    var dt: Int
+    var main: Main
+    var weather: [Weather]
+    var clouds: Clouds
+    var wind: Wind
+    var visibility: Int
+    var pop: Double
+    var rain: Rain?
+    var snow: Snow?
+    var sys: List.Sys
+    var dateText: String
+    
+    struct Sys: Decodable {
+        var pod: String
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case dt, main, weather, clouds, wind, visibility, pop, rain, snow, sys
+        case dateText = "dt_txt"
+    }
+}
+
+// MARK: - City
+
+struct City: Decodable {
+    var id: Int
+    var name: String
+    var coord: Coord
+    var country: String
+    var population: Int
+    var timezone: Int
+    var sunrise: Int
+    var sunset: Int
+}
+
+
