75229fc7f5012b150d4ba28aa522cd3298d27faa
dpwns1234 <52391722+dpwns1234@users.noreply.github.com>
2023-11-21 16:00:11 +0900

    refactor: URLSession을 delegate 패턴을 사용해 리팩터

--- WeatherForecast/WeatherForecast/Network/NetworkManager.swift
+++ WeatherForecast/WeatherForecast/Network/NetworkManager.swift
@@ -7,10 +7,16 @@
 
 import Foundation
 
-struct NetworkManager: ApiClient {
+class NetworkManager: NSObject {
     var components = URLComponents(string: "https://api.openweathermap.org/data/2.5/")
     
-    init() {
+    private lazy var session: URLSession = {
+        let configuration = URLSessionConfiguration.default
+        configuration.waitsForConnectivity = true
+        return URLSession(configuration: configuration, delegate: self, delegateQueue: nil)
+    }()
+    
+    override init() {
         components?.path += "forecast/"
         let apiKey = URLQueryItem(name: "appid", value: Sequrity.weatherApiKey)
         components?.queryItems?.append(apiKey)
@@ -22,5 +28,42 @@ struct NetworkManager: ApiClient {
         let lonQueryItem = URLQueryItem(name: "lon", value: String(lon))
         let apiKey = URLQueryItem(name: "appid", value: Sequrity.weatherApiKey)
         components?.queryItems = [latQueryItem, lonQueryItem, apiKey]
+    }
+    
+    func startLoad() {
+        guard let url = self.components?.url else { return }
+        let task = session.dataTask(with: url)
+        task.resume()
+    }
+}
+
+// MARK: - URLSessionDataDelegate
+
+extension NetworkManager: URLSessionDataDelegate {
+    
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping @Sendable (URLSession.ResponseDisposition) -> Void) {
+        guard
+            let response = response as? HTTPURLResponse,
+            (200...299).contains(response.statusCode),
+            let mimeType = response.mimeType,
+            mimeType == "application/json"
+        else {
+            completionHandler(.cancel)
+            return
+        }
+        
+        completionHandler(.allow)
+    }
+    
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        let weatherForecast = try! JSONDecoder().decode(WeatherToday.self, from: data)
+        print(weatherForecast)
+    }
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
     }
 }
--- WeatherForecast/WeatherForecast/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -6,48 +6,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, URLSessionDelegate, URLSessionTaskDelegate {
-        
+class ViewController: UIViewController {
+    private let networkManager = NetworkManager(lat: 37.532600, lon: 127.024612)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        guard let weatherURL = NetworkManager(lat: 37.532600, lon: 127.024612).components?.url else { return }
-        let request = URLRequest(url: weatherURL)
-        
-        let session = URLSession(configuration: .default)
-        let task = session.dataTask(with: request) { (data, response, error) in
-            if let error = error {
-                print("Error: \(error.localizedDescription)")
-                return
-            }
-            
-            guard 
-                let httpResponse = response as? HTTPURLResponse,
-                  (200...299).contains(httpResponse.statusCode) 
-            else {
-                print("Error: \(String(describing: response))")
-                return
-            }
-            
-            guard let data = data else {
-                print("No data received")
-                return
-            }
-            
-            do {
-                let json = try JSONSerialization.jsonObject(with: data, options: [])
-                //print(json)
-                let weatherForecast = try! JSONDecoder().decode(WeatherToday.self, from: data)
-                print(weatherForecast)
-            } catch let error {
-                print("Error: \(error.localizedDescription)")
-            }
-            
-        }
-        
-        task.resume()
-        
+        networkManager.startLoad()
     }
-
-
 }
