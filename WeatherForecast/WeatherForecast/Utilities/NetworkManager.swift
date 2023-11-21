94b7ec7aa5024a28382ab0a0536b5615630a4e8c
YunSwain <destap@naver.com>
2023-11-21 10:08:21 +0900

    refactor: NetworkManager 기능 분리, DataService 클래스 추가

--- WeatherForecast/WeatherForecast/Utilities/NetworkManager.swift
+++ WeatherForecast/WeatherForecast/Utilities/NetworkManager.swift
@@ -7,32 +7,47 @@
 
 import Foundation
 
+protocol DataServiceProtocol {
+    func fetchData(_ serviceType: ServiceType)
+}
+
+final class WeatherDataService: DataServiceProtocol {
+    private var weatherModel: WeatherModel? = nil
+    private let networkManager: NetworkManager
+    
+    init(networkManager: NetworkManager) {
+        self.networkManager = networkManager
+    }
+    
+    func fetchData(_ serviceType: ServiceType) {
+        guard let apiKey = Bundle.getAPIKey(for: ApiName.openWeatherMap.name) else { return }
+        let urlString = "https://api.openweathermap.org/data/2.5/\(serviceType)?lat=32.98&lon=44.11&appid=\(apiKey)&units=metric&lang=kr"
+        
+        guard let downloadedData = networkManager.downloadData(urlString: urlString) else { return }
+        weatherModel = try? JSONDecoder().decode(WeatherModel.self, from: downloadedData)
+    }
+}
+
 final class NetworkManager {
-    static let `default` = NetworkManager() // singleton
-    private let openWeatherMapKey: String = ApiName.openWeatherMap.name
-    var weatherModel: WeatherModel?
+    static let `default` = NetworkManager()
     
     private init() {}
     
-    func downloadData(urlString: String) -> WeatherModel? {
+    func downloadData(urlString: String) -> Data? {
         guard let url = URL(string: urlString) else {
             return nil
         }
         
-        let request = URLRequest(url: url)
-        URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+        var downloadedData: Data? = nil
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
             guard let data = data, let response = response as? HTTPURLResponse,
                   response.statusCode >= 200 && response.statusCode < 300
-            else {
-                return
-            }
+            else { return }
             
-            self?.weatherModel = try? JSONDecoder().decode(WeatherModel.self, from: data)
-            
+            downloadedData = data
         }.resume()
         
-        return weatherModel
+        return downloadedData
     }
-    
-    
 }
