7b7471d32cfb0eb322dd1e3c3a658604602e1d3a
JinUng41 <ingjwjw@naver.com>
2023-11-21 13:45:19 +0900

    refactor: 관측 정보에 따른 URL을 리턴하도록 구현

---
+++ WeatherForecast/WeatherForecast/Models/WeatherType.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherURL.swift
+//  WeatherForecast
+//
+//  Created by 김경록 on 11/21/23.
+//
+
+import Foundation
+
+enum WeatherType: String {
+    case current = "weather"
+    case forecast = "forcast"
+    
+    func fetchURL(lon: Double, lat: Double) -> String {
+        guard let apiKey = Bundle.main.apiKey else {
+            print("API KEY를 찾을 수 없습니다!!")
+            return ""
+        }
+        return "https://api.openweathermap.org/data/2.5/\(self.rawValue)?lat=\(lon)&lon=\(lat)&appid=\(apiKey)"
+    }
+}
