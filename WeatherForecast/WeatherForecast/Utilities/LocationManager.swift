3d66d36debbef15bcdb1a6b3ec4fdf353b4c8cba
YunSwain <destap@naver.com>
2023-11-21 10:08:00 +0900

    feat: LocationManager 클래스 추가

--- WeatherForecast/WeatherForecast/Utilities/LocationManager.swift
+++ WeatherForecast/WeatherForecast/Utilities/LocationManager.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import CoreLocation
+
+final class LocationManager: NSObject {
+    static let shared = LocationManager()
+    
+    private let locationManager: CLLocationManager = CLLocationManager()
+    private var location: CLLocation?
+    
+    private override init() {
+        super.init()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        self.location = location
+    }
+}
