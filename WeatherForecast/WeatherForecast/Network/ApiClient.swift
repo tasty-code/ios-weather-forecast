15f2015de22af05979b254b383df04f424aeebf6
dpwns1234 <52391722+dpwns1234@users.noreply.github.com>
2023-11-22 15:24:28 +0900

    feat: api를 다루는 ApiClient 구현

--- WeatherForecast/WeatherForecast/Network/ApiClient.swift
+++ WeatherForecast/WeatherForecast/Network/ApiClient.swift
@@ -7,6 +7,17 @@
 
 import Foundation
 
-protocol ApiClient {
-    var components: URLComponents? { get } // TODO: 왜 set 안해도 돌아가는가?
+struct ApiClient {
+    private static let baseURL = "https://api.openweathermap.org/data/2.5/"
+    
+    static func makeURL(lat: Double, lon: Double, dataType: DataType) -> URL? {
+        var components = URLComponents(string: baseURL)
+        components?.path += dataType.description
+        let latQueryItem = URLQueryItem(name: "lat", value: String(lat))
+        let lonQueryItem = URLQueryItem(name: "lon", value: String(lon))
+        let apiKey = URLQueryItem(name: "appid", value: Sequrity.weatherApiKey)
+        components?.queryItems = [latQueryItem, lonQueryItem, apiKey]
+        
+        return components?.url
+    }
 }
