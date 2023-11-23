73cc29f4bf3b03ad0870a82186494e9781b2c996
kimjunsung <assd1108@naver.com>
2023-11-23 10:24:29 +0900

    [refactor] status code fatalError대신 에러를 던지도록 수정

--- WeatherForecast/WeatherForecast/Utils/Errors.swift
+++ WeatherForecast/WeatherForecast/Utils/Errors.swift
@@ -19,3 +19,43 @@ enum APIKeyError : Error, CustomStringConvertible {
         }
     }
 }
+
+enum StatusCodeError: Error {
+    case badRequest
+    case unauthorized
+    case forbidden
+    case notFound
+    case notAcceptable
+    case other4XXError(statusCode: Int)
+    case internalSeverError
+    case badGateway
+    case gatewayTimeout
+    case other5XXError(statusCode: Int)
+    case anotherStatusError(statusCode: Int)
+    
+    static func httpError(_ statusCode: Int) -> StatusCodeError {
+        switch statusCode {
+        case 400:
+            return .badRequest
+        case 401:
+            return .unauthorized
+        case 403:
+            return .forbidden
+        case 406:
+            return .notAcceptable
+        case 400..<500:
+            return .other4XXError(statusCode: statusCode)
+        case 500:
+            return .internalSeverError
+        case 502:
+            return .badGateway
+        case 504:
+            return .gatewayTimeout
+        case 500..<600:
+            return .other5XXError(statusCode: statusCode)
+        default:
+            return .anotherStatusError(statusCode: statusCode)
+        }
+    }
+}
+
