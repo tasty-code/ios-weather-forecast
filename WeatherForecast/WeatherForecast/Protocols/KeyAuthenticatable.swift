d28c4be7dc9ac86470df4f89b86a46a1b3fd10c5
JJong <jfknfj920@naver.com>
2023-11-23 20:55:05 +0900

    refactor: APIKey를 별도 프로토콜로 분리하고 private로 설정

--- WeatherForecast/WeatherForecast/Protocols/KeyAuthenticatable.swift
+++ WeatherForecast/WeatherForecast/Protocols/KeyAuthenticatable.swift
@@ -1,3 +1,30 @@
+import Foundation
+
 protocol KeyAuthenticatable {
-    var apiKey: String { get throws }
+    
+}
+
+extension KeyAuthenticatable {
+    private func checkAPIKey() -> Result<String, APIError> {
+        guard let filePath = Bundle.main.path(forResource: "APIKeyList", ofType: "plist") else {
+            return .failure(.noExistedAPIPlist)
+        }
+        
+        if let plist = NSDictionary(contentsOfFile: filePath),
+           let value = plist.object(forKey: "OPENWEATHERMAP_KEY") as? String {
+            return .success(value)
+        } else {
+            return .failure(.noExistedAPIKey)
+        }
+    }
+    
+    var APIKey: String? {
+        switch checkAPIKey() {
+        case .success(let APIKey):
+            return APIKey
+        case .failure(let error):
+            print(error)
+            return nil
+        }
+    }
 }
