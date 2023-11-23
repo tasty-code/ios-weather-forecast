1645565fcbf12f6adc87f3c3212302f29f47e924
JJong <jfknfj920@naver.com>
2023-11-23 13:28:15 +0900

    feat: apiKey를 다룰 KeyAuthenticatable 프로토콜 추가

--- WeatherForecast/WeatherForecast/Protocols/KeyAuthenticatable.swift
+++ WeatherForecast/WeatherForecast/Protocols/KeyAuthenticatable.swift
@@ -1,12 +1,3 @@
-//
-//  KeyAuthenticatable.swift
-//  WeatherForecast
-//
-//  Created by 동준 on 11/23/23.
-//
-
-import Foundation
-
 protocol KeyAuthenticatable {
     var apiKey: String { get throws }
 }
