1e4fb4df72287628b42dcfbb1b20bc629cca5447
JinUng41 <ingjwjw@naver.com>
2023-11-24 14:52:29 +0900

    feat: API 요청을 만들고 네트워크 작업을 실제 수행
    - 프로토콜을 채택하고 실제 동작을 정의
    - API 요청을 만들고, 실제 네트워크 작업을 수행

--- WeatherForecast/WeatherForecast/Controllers/ViewController.swift
+++ WeatherForecast/WeatherForecast/Controllers/ViewController.swift
@@ -5,10 +5,30 @@
 // 
 
 import UIKit
+import CoreLocation
 
 final class ViewController: UIViewController {
+    private let locationManager: LocationManager = LocationManager()
+    private let networkManager: NetworkManager = NetworkManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.delegate = self
     }
 }
 
+extension ViewController: LocationManagerDelegate {
+    func update(with info: LocationData) {
+        let weatherRequest = WeatherRequest(latitude: info.latitude,
+                                            longitude: info.longitude,
+                                            weatherType: .current)
+        networkManager.fetchData(for: weatherRequest) { (result: Result<Current, Error>) in
+            switch result {
+            case .success(let weather):
+                print(weather)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
