a426c0e902d8b6dec04aed944388680fa9f0acb4
bamsak <qjatjrdl963@naver.com>
2023-11-24 12:53:12 +0900

    [Chore] API DTO Nested Type으로 변경 및 공통된 DTO는 NameSpace를 통해 분리

--- WeatherForecast/WeatherForecast/Model/CurrentWeather.swift
+++ WeatherForecast/WeatherForecast/Model/CurrentWeather.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 struct CurrentWeather: Decodable {
-    var coordinate: Coordinate?
-    var weather: [Weather]?
+    var coordinate: CommonWeatherDTO.Coordinate?
+    var weather: [CommonWeatherDTO.Weather]?
     var base: String?
-    var main: Main?
+    var main: CommonWeatherDTO.Main?
     var visibility: Int?
-    var wind: Wind?
-    var clouds: Clouds?
-    var rain: Rain?
-    var snow: Snow?
+    var wind: CommonWeatherDTO.Wind?
+    var clouds: CommonWeatherDTO.Clouds?
+    var rain: CommonWeatherDTO.Rain?
+    var snow: CommonWeatherDTO.Snow?
     var dataTime: Int?
     var system: System?
     var timezone: Int?
@@ -30,102 +30,12 @@ struct CurrentWeather: Decodable {
         case dataTime = "dt"
         case system = "sys"
     }
-}
-
-// MARK: - Coord
-
-struct Coordinate: Decodable {
-    var longitude: Double
-    var latitude: Double
     
-    enum CodingKeys: String, CodingKey {
-        case longitude = "lon"
-        case latitude = "lat"
+    struct System: Decodable {
+        var type: Int?
+        var id: Int?
+        var country: String?
+        var sunrise: Int?
+        var sunset: Int?
     }
 }
-
-// MARK: - Weather
-
-struct Weather: Decodable {
-    var id: Int?
-    var main: String?
-    var description: String?
-    var icon: String?
-}
-
-// MARK: - Main
-
-struct Main: Decodable {
-    var temperature: Double?
-    var feelsLike: Double?
-    var minTemperature: Double?
-    var maxTemperature: Double?
-    var pressure: Int?
-    var humidity: Int?
-    var seaLevel: Int?
-    var groundLevel: Int?
-    
-    enum CodingKeys: String, CodingKey {
-        case pressure, humidity
-        case temperature = "temp"
-        case feelsLike = "feels_like"
-        case minTemperature = "temp_min"
-        case maxTemperature = "temp_max"
-        case seaLevel = "sea_level"
-        case groundLevel = "grnd_level"
-    }
-}
-
-// MARK: - Wind
-
-struct Wind: Decodable {
-    var speed: Double?
-    var direction: Int?
-    var gust: Double?
-    
-    enum CodingKeys: String, CodingKey {
-        case speed, gust
-        case direction = "deg"
-    }
-}
-
-// MARK: - Clouds
-
-struct Clouds: Decodable {
-    var all: Int?
-}
-
-// MARK: - Rain
-
-struct Rain: Decodable {
-    var oneHour: Double?
-    var threeHour: Double?
-    
-    enum CodingKeys: String, CodingKey {
-        case oneHour = "1h"
-        case threeHour = "3h"
-    }
-}
-
-// MARK: - Snow
-
-struct Snow: Decodable {
-    var oneHour: Double?
-    var threeHour: Double?
-    
-    enum CodingKeys: String, CodingKey {
-        case oneHour = "1h"
-        case threeHour = "3h"
-    }
-}
-
-// MARK: - Sys
-
-struct System: Decodable {
-    var type: Int?
-    var id: Int?
-    var country: String?
-    var sunrise: Int?
-    var sunset: Int?
-}
-
--- WeatherForecast/WeatherForecast/Model/WeeklyWeather.swift
+++ WeatherForecast/WeatherForecast/Model/WeeklyWeather.swift
@@ -18,56 +18,51 @@ struct WeeklyWeather: Decodable {
         case cod, message, list, city
         case timestampCount = "cnt"
     }
-}
-
-// MARK: - List
-
-struct List: Decodable {
-    var dataTime: Int?
-    var main: Main?
-    var weather: [Weather]?
-    var clouds: Clouds?
-    var wind: Wind?
-    var visibility: Int?
-    var probabilityOfPrecipitation: Double?
-    var rain: Rain?
-    var snow: Snow?
-    var system: System?
-    var dateText: String?
     
-    struct System: Decodable {
-        var partOfTheDay: String?
+    struct List: Decodable {
+        var dataTime: Int?
+        var main: CommonWeatherDTO.Main?
+        var weather: [CommonWeatherDTO.Weather]?
+        var clouds: CommonWeatherDTO.Clouds?
+        var wind: CommonWeatherDTO.Wind?
+        var visibility: Int?
+        var probabilityOfPrecipitation: Double?
+        var rain: CommonWeatherDTO.Rain?
+        var snow: CommonWeatherDTO.Snow?
+        var system: System?
+        var dateText: String?
         
-        enum CodingKeys:String, CodingKey {
-            case partOfTheDay = "pod"
+        struct System: Decodable {
+            var partOfTheDay: String?
+            
+            enum CodingKeys:String, CodingKey {
+                case partOfTheDay = "pod"
+            }
+        }
+        
+        enum CodingKeys: String, CodingKey {
+            case main, weather, clouds, wind, visibility, rain, snow
+            case dataTime = "dt"
+            case probabilityOfPrecipitation = "pop"
+            case system = "sys"
+            case dateText = "dt_txt"
         }
     }
-    
-    enum CodingKeys: String, CodingKey {
-        case main, weather, clouds, wind, visibility, rain, snow
-        case dataTime = "dt"
-        case probabilityOfPrecipitation = "pop"
-        case system = "sys"
-        case dateText = "dt_txt"
+
+    struct City: Decodable {
+        var id: Int?
+        var name: String?
+        var coordinate: CommonWeatherDTO.Coordinate?
+        var country: String?
+        var population: Int?
+        var timezone: Int?
+        var sunrise: Int?
+        var sunset: Int?
+        
+        enum CodingKeys: String, CodingKey {
+            case id, name, country, population, timezone, sunrise, sunset
+            case coordinate = "coord"
+        }
     }
 }
-
-// MARK: - City
-
-struct City: Decodable {
-    var id: Int?
-    var name: String?
-    var coordinate: Coordinate?
-    var country: String?
-    var population: Int?
-    var timezone: Int?
-    var sunrise: Int?
-    var sunset: Int?
-    
-    enum CodingKeys: String, CodingKey {
-        case id, name, country, population, timezone, sunrise, sunset
-        case coordinate = "coord"
-    }
-}
-
 
