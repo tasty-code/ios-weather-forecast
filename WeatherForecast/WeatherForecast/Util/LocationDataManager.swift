80febbdbb30681241295f7a4618ae428d211fcbc
dpwns1234 <52391722+dpwns1234@users.noreply.github.com>
2023-11-24 16:56:48 +0900

    feat: CoreLocation을 통해 주소정보 가져오는 기능 구현

--- WeatherForecast/WeatherForecast/Util/LocationDataManager.swift
+++ WeatherForecast/WeatherForecast/Util/LocationDataManager.swift
@@ -53,6 +53,12 @@ extension LocationDataManager: CLLocationManagerDelegate {
         if let coordinate = locations.last?.coordinate {
             print(coordinate)
             locationDelegate?.location(self, didLoad: coordinate)
+            lookUpCurrentLocation { placemarks in
+                if let placemarks = placemarks {
+                    let address = "\(placemarks.country!) \(placemarks.administrativeArea!) \(placemarks.locality!) \(placemarks.subLocality!) \(placemarks.thoroughfare!) \(placemarks.subThoroughfare!)"
+                    print(address)
+                }
+            }
         }
         
         locationManager.stopUpdatingLocation()
@@ -61,5 +67,23 @@ extension LocationDataManager: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("GPS 사용이 불가한 지역에 있는 등 위치 정보를 가져오지 못했습니다.")
     }
-    
+}
+
+extension LocationDataManager {
+    func lookUpCurrentLocation(completionHandler: @escaping (CLPlacemark?) -> Void ) {
+        if let lastLocation = locationManager.location {
+            let geocoder = CLGeocoder()
+            
+            geocoder.reverseGeocodeLocation(lastLocation) { (placemrks, error) in
+                if error == nil {
+                    let firstLocation = placemrks?[0]
+                    completionHandler(firstLocation)
+                } else {
+                    completionHandler(nil)
+                }
+            }
+        } else {
+            completionHandler(nil)
+        }
+    }
 }
