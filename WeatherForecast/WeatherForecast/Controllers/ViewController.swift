6c538b90e6bd13742551c42161be1e693f7e226c
JinUng41 <ingjwjw@naver.com>
2023-11-25 21:25:43 +0900

    test: 기능 확인을 위한 코드 삽입

--- WeatherForecast/WeatherForecast/Controllers/ViewController.swift
+++ WeatherForecast/WeatherForecast/Controllers/ViewController.swift
@@ -18,11 +18,25 @@ final class ViewController: UIViewController {
 }
 
 extension ViewController: LocationManagerDelegate {
-    func update(with info: LocationData) {
-        let weatherRequest = WeatherRequest(latitude: info.latitude,
-                                            longitude: info.longitude,
+    func update(with data: LocationData) {
+        printLocationData(data)
+        let weatherRequest = WeatherRequest(latitude: data.latitude,
+                                            longitude: data.longitude,
                                             weatherType: .current)
         networkManager.fetchData(for: weatherRequest) { (result: Result<Current, Error>) in
+            switch result {
+            case .success(let weather):
+                print(weather)
+                print("")
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
+        let forecastRequest = WeatherRequest(latitude: data.latitude,
+                                            longitude: data.longitude,
+                                            weatherType: .forecast)
+        networkManager.fetchData(for: forecastRequest) { (result: Result<Forecast, Error>) in
             switch result {
             case .success(let weather):
                 print(weather)
@@ -30,5 +44,11 @@ extension ViewController: LocationManagerDelegate {
                 print(error)
             }
         }
+    }
+}
+
+extension ViewController {
+    private func printLocationData(_ data: LocationData) {
+        print("위도: \(data.latitude) 경도: \(data.longitude) 주소: \(data.address) \n")
     }
 }
