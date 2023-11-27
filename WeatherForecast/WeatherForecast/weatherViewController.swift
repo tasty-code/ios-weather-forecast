1bffac054c2d4c71fca306ad947217b7074d8f6a
SimJaeHyeok <jaehyeoking@gmail.com>
2023-11-27 13:22:48 +0900

    feat: 위치 권한 허용 및 위도, 경도 확인 기능 추가

--- WeatherForecast/WeatherForecast/weatherViewController.swift
+++ WeatherForecast/WeatherForecast/weatherViewController.swift
@@ -1,11 +1,25 @@
 import UIKit
+import CoreLocation
 
 class WeatherViewController: UIViewController {
+    let location = CLLocationManager()
+    var latitude = 0.0
+    var longitude = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        location.delegate = self
+        location.requestWhenInUseAuthorization()
+        location.startUpdatingLocation()
     }
 }
 
-
-
+extension WeatherViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+          latitude = location.coordinate.latitude
+          longitude = location.coordinate.longitude
+        }
+        print(latitude, longitude)
+    }
+}
