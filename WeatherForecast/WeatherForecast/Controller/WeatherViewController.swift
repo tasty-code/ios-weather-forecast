cd4889d70d79c6ffb82d5595371bc676a66ae694
dpwns1234 <52391722+dpwns1234@users.noreply.github.com>
2023-11-29 15:09:40 +0900

    refactor: lookUpCurrentAddress error처리
    - current loaction을 찾는 메서드에서 주소를 찾지 못할 경우 handler의 인자로 nil을 전달하고, 인자(placemark)가 nil이라면 에러 처리를 해준다.

--- WeatherForecast/WeatherForecast/Controller/WeatherViewController.swift
+++ WeatherForecast/WeatherForecast/Controller/WeatherViewController.swift
@@ -6,7 +6,6 @@
 
 import UIKit
 import CoreLocation
-
 final class WeatherViewController: UIViewController {
     private let networkManager = WeatherNetworkManager()
     private let locationDataManager = LocationDataManager()
@@ -26,20 +25,21 @@ extension WeatherViewController: LocationDataManagerDelegate {
     }
     
     func viewCurrentAddress(placemark: CLPlacemark?) {
-        if let placemark {
-            if
-                let country = placemark.country,
-                let administrativeArea = placemark.administrativeArea,
-                let locality = placemark.locality,
-                let subLocality = placemark.subLocality,
-                let thoroughfare = placemark.thoroughfare,
-                let subThoroughfare = placemark.subThoroughfare
-            {
-                let address = "\(country) \(administrativeArea) \(locality) \(subLocality) \(thoroughfare) \(subThoroughfare)"
-                print(address)
-            }
-        } else {
-            print("location 가져오기 실패")
+        guard let placemark else {
+            print("can't look up current address")
+            return
+        }
+        
+        if
+            let country = placemark.country,
+            let administrativeArea = placemark.administrativeArea,
+            let locality = placemark.locality,
+            let subLocality = placemark.subLocality,
+            let thoroughfare = placemark.thoroughfare,
+            let subThoroughfare = placemark.subThoroughfare
+        {
+            let address = "\(country) \(administrativeArea) \(locality) \(subLocality) \(thoroughfare) \(subThoroughfare)"
+            print(address)
         }
     }
 }
--- WeatherForecast/WeatherForecast/Util/LocationDataManager.swift
+++ WeatherForecast/WeatherForecast/Util/LocationDataManager.swift
@@ -47,7 +47,6 @@ extension LocationDataManager: CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        // TODO: 에러처리
         guard let locationDelegate else { return }
         if let coordinate = locations.last?.coordinate {
             locationDelegate.location(self, didLoad: coordinate)
@@ -66,20 +65,17 @@ extension LocationDataManager: CLLocationManagerDelegate {
 
 extension LocationDataManager {
     private func lookUpCurrentAddress(completionHandler: @escaping (CLPlacemark?) -> Void ) {
-        if let lastLocation = locationManager.location {
-            let geocoder = CLGeocoder()
-            
-            geocoder.reverseGeocodeLocation(lastLocation) { (placemarks, error) in
-                if error == nil {
-                    guard let placemarks else { return }
-                    let firstLocation = placemarks[0]
-                    completionHandler(firstLocation)
-                } else {
-                    completionHandler(nil)
-                }
+        guard let lastLocation = locationManager.location else { return completionHandler(nil) }
+        let geocoder = CLGeocoder()
+        
+        geocoder.reverseGeocodeLocation(lastLocation) { (placemarks, error) in
+            if error == nil {
+                guard let placemarks else { return completionHandler(nil) }
+                let firstLocation = placemarks[0]
+                completionHandler(firstLocation)
+            } else {
+                completionHandler(nil)
             }
-        } else {
-            completionHandler(nil)
         }
     }
 }
