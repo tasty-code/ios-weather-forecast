24b57ad09465080f355166b2d232466673ba1b22
imSeongHyeon <jfknfj920@naver.com>
2023-11-30 17:18:01 +0900

    refactor: LocationManager -> VC: delegate -> completion handler

--- WeatherForecast/WeatherForecast/ViewControllers/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewControllers/ViewController.swift
@@ -1,27 +1,29 @@
 import UIKit
 import CoreLocation
 
-final class ViewController: UIViewController, CLLocationManagerDelegate {
+final class ViewController: UIViewController {
     private let locationManager = LocationManager.shared
-    private let networker = Networker<Model.CurrentWeather>(request: WeatherAPI.current)
+    private var model: Decodable?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        locationManager.delegate = self
-    }
-}
-
-extension ViewController: UILocationDelegate {
-    func update(placemark: CLPlacemark) {
-        print(placemark)
-    }
-    
-    func update(coordinate: CLLocationCoordinate2D?) {
-        print(coordinate)
-        
-        networker.fetchWeatherData { weatherResponse in
-            print(weatherResponse)
+        locationManager.request { [self] result in
+            switch result {
+            case.success((let coordinate, let placemark)):
+                print(coordinate)
+                print(placemark)
+                
+                let networker = Networker<Model.CurrentWeather>(request: WeatherAPI.current)
+                
+                networker.fetchWeatherData { [weak self] weatherResponse in
+                    self?.model = weatherResponse
+                    print(self?.model)
+                    //        model 받아서 ColletionView로 넘겨준다.
+                }
+            case .failure(let error):
+                print(error)
+            }
         }
     }
 }
