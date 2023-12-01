b9ad6af8ec0a54d25585ed1a07502a5482a301d2
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2023-12-01 17:40:49 +0900

    [Feat] 쿼리 생성 기능 구현 및 에러 케이스 생성

--- WeatherForecast/WeatherForecast/Utility/CurrentLocationManager.swift
+++ WeatherForecast/WeatherForecast/Utility/CurrentLocationManager.swift
@@ -9,7 +9,26 @@ import CoreLocation
 
 final class CurrentLocationManager {
     private var locationInfo: CurrentLocationInfo?
-
+    
+    private func makeQueries() throws -> [String: String] {
+        guard let longitude = locationInfo?.coordinates?.longitude,
+              let latitude = locationInfo?.coordinates?.latitude
+        else {
+            throw QueryError.noneCoordinate
+        }
+        guard let appid = Bundle.main.apiKey
+        else {
+            throw QueryError.noneAPIKey
+        }
+        
+        let queries = [
+            "lon": "\(longitude)",
+            "lat": "\(latitude)",
+            "appid": appid
+        ]
+        
+        return queries
+    }
 }
 
 extension CurrentLocationManager: CurrentLocationManagable {
