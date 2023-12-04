ffdd1deadc24450978468bd0381e269779e8a82d
JJong <jfknfj920@naver.com>
2023-12-04 10:05:02 +0900

    refactor: VC내 network를 전역 변수로 변경

--- WeatherForecast/WeatherForecast/ViewControllers/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewControllers/ViewController.swift
@@ -4,19 +4,20 @@ import CoreLocation
 final class ViewController: UIViewController {
     private let locationManager = LocationManager()
     private var model: Decodable?
+    private var networker: Networker<Model.CurrentWeather>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        locationManager.request { result in
+        locationManager.request { [self] result in
             switch result {
             case.success((let coordinate, let placemark)):
                 print(coordinate)
                 print(placemark)
                 
-                let networker = Networker<Model.CurrentWeather>(request: WeatherAPI.current(coordinate))
+                networker = Networker<Model.CurrentWeather>(request: WeatherAPI.current(coordinate))
                 
-                networker.fetchWeatherData { [weak self] weatherResponse in
+                networker?.fetchWeatherData { [weak self] weatherResponse in
                     self?.model = weatherResponse
                     print(self?.model)
                 }
