b2c961f705de3c31e3b33ff5c6d1a0c95b1828b2
dpwns1234 <52391722+dpwns1234@users.noreply.github.com>
2023-12-08 15:02:39 +0900

    refactor: weatherNetworkManager 수정

--- WeatherForecast/WeatherForecast/Network/WeatherNetworkManager.swift
+++ WeatherForecast/WeatherForecast/Network/WeatherNetworkManager.swift
@@ -1,71 +1,33 @@
-//
-//  NetworkManager.swift
-//  WeatherForecast
-//
-//  Created by 김예준 on 11/21/23.
+////
+////  WeatherNetworkManager.swift
+////  WeatherForecast
+////
+////  Created by 김예준 on 11/21/23.
+////
 //
 
 import Foundation
 import CoreLocation
 
-final class WeatherNetworkManager<T: Decodable>: NSObject, URLSessionDelegate, URLSessionDataDelegate {
-    static var shared: WeatherNetworkManager<T> {
-        return WeatherNetworkManager<T>()
-    }
-    typealias ResponseClosure = (T) -> Void
-    var responseClosure: ResponseClosure?
-    
-    private var receivedData: Data?
-    private lazy var session: URLSession = {
-        let configuration = URLSessionConfiguration.default
-        configuration.waitsForConnectivity = true
-        return URLSession(configuration: configuration, delegate: self, delegateQueue: nil)
-    }()
-    
-    func loadWeatherData(type: WeatherType, coord: CLLocationCoordinate2D) {
-        do {
-            let request = try WeatherApiClient.makeRequest(weatherType: type, coord: coord)
-            let task = session.dataTask(with: request)
-            task.resume()
-        } catch {
-            print(error)
-        }
-    }
-    
-    // MARK: - URLSessionDataDelegate
-    
-    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping @Sendable (URLSession.ResponseDisposition) -> Void) {
-        guard
-            let response = response as? HTTPURLResponse,
-            (200...299).contains(response.statusCode)
-        else {
-            completionHandler(.cancel)
-            return
-        }
-        
-        receivedData = Data()
-        completionHandler(.allow)
-    }
-    
-    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
-        receivedData?.append(data)
-    }
-    
-    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
-        if let error = error {
-            print(error.localizedDescription)
-            return
-        }
-        
-        if let receivedData = self.receivedData {
-            do {
-                let data = try JSONDecoder().decode(T.self, from: receivedData)
-                if let responseClosure {
-                    responseClosure(data)
-                }
-            } catch {
-                print(error)
+final class WeatherNetworkManager {
+    func downloadData(url: URL, completionHandler: @escaping (Result<Data, Error>) -> Void) {
+        let request = URLRequest(url: url)
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if error == nil {
+                completionHandler(.failure(NetworkError.failedTask))
             }
-        }
+            guard
+                let response = response as? HTTPURLResponse,
+                (200...299).contains(response.statusCode)
+            else {
+                return completionHandler(.failure(NetworkError.notSuccessCode))
+            }
+            
+            guard let data = data else {
+                return completionHandler(.failure(NetworkError.failedToLoadData))
+            }
+            
+            completionHandler(.success(data))
+        }.resume()
     }
 }
