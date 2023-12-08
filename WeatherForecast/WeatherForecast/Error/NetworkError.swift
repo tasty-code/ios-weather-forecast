8a4d0b0cdf0e6d039e642b107c084a89fd0d617e
dpwns1234 <52391722+dpwns1234@users.noreply.github.com>
2023-12-08 14:57:22 +0900

    feat: error 타입 추가

--- WeatherForecast/WeatherForecast/Error/NetworkError.swift
+++ WeatherForecast/WeatherForecast/Error/NetworkError.swift
@@ -9,11 +9,20 @@ import Foundation
 
 enum NetworkError: Error, CustomStringConvertible {
     case invailedURL
-    
+    case failedToLoadData
+    case notSuccessCode
+    case failedTask
     var description: String {
         switch self {
         case .invailedURL:
             "API url이 유효하지 않습니다."
+        case .failedToLoadData:
+            "data를 받아오지 못했습니다."
+        case .notSuccessCode:
+            "200번대 코드가 아닙니다."
+        case .failedTask:
+            "dataTask에 실패하였습니다."
         }
+    
     }
 }
