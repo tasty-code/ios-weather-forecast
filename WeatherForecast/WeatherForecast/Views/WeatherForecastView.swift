4eb8282b356887a3f1a293cac682cb21605f64ec
imSeongHyeon <jfknfj920@naver.com>
2023-12-08 15:25:36 +0900

    feat: 날씨 UI 구현

--- WeatherForecast/WeatherForecast/Views/WeatherForecastView.swift
+++ WeatherForecast/WeatherForecast/Views/WeatherForecastView.swift
@@ -1,42 +1,67 @@
 import UIKit
+import CoreLocation
 
 final class WeatherForecastView: UIView {
+    
     let collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
+        let layout = UICollectionViewCompositionalLayout { section, environment in
+            //            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(100))
+            //            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1))
+            //            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            var configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+            configuration.headerMode = .supplementary
+            configuration.backgroundColor = .clear
+            
+            let section = NSCollectionLayoutSection.list(using: configuration, layoutEnvironment: environment)
+            section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10)
+            
+            let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.15))
+            let header = NSCollectionLayoutBoundarySupplementaryItem(
+                layoutSize: headerSize,
+                elementKind: UICollectionView.elementKindSectionHeader,
+                alignment: .top)
+            section.boundarySupplementaryItems = [header]
+            
+            return section
+        }
         
-        layout.scrollDirection = .vertical
-        layout.itemSize = CGSize(width: 500, height: 100)
-        
-        let collectionView = UICollectionView(frame: .zero,
-                                              collectionViewLayout: layout)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.register(WeatherForecastCell.self,
-                                forCellWithReuseIdentifier: WeatherForecastCell.identifier)
-        collectionView.register(WeatherForecastHeaderView.self,
-                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
-                                withReuseIdentifier: WeatherForecastHeaderView.identifier)
+        collectionView.register(WeatherForecastCell.self, forCellWithReuseIdentifier: WeatherForecastCell.identifier)
+        collectionView.register(
+            WeatherForecastHeaderView.self,
+            forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: WeatherForecastHeaderView.identifier
+        )
         
         return collectionView
     }()
     
+    private var currentWeathermodel: Model.CurrentWeather?
+    private var fiveDaysWeatherModel: Model.FiveDaysWeather?
+    private var locality: String?
+    private var subLocality: String?
+    private var placemark: CLPlacemark?
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setUI()
-        setConstraint()
+        configureUI()
+        configureConstraint()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
     
-    private func setUI() {
-        self.backgroundColor = .blue
+    private func configureUI() {
+        self.backgroundColor = .clear
         self.addSubview(collectionView)
+        collectionView.backgroundView = UIImageView(image: UIImage(named: "backgroundImage"))
         collectionView.delegate = self
         collectionView.dataSource = self
     }
     
-    private func setConstraint() {
+    private func configureConstraint() {
         let safeArea = self.safeAreaLayoutGuide
         NSLayoutConstraint.activate([
             collectionView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
@@ -45,66 +70,74 @@ final class WeatherForecastView: UIView {
             collectionView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
         ])
     }
+    
+    func sendCurrentWeatherModel(model: Model.CurrentWeather, placemark: CLPlacemark) {
+        currentWeathermodel = model
+        self.placemark = placemark
+    }
+    
+    func sendFiveDaysWeatherModel(model: Model.FiveDaysWeather) {
+        fiveDaysWeatherModel = model
+    }
+    
+    func sendLocation(_ locality: String?, _ subLocality: String?) {
+        self.locality = locality
+        self.subLocality = subLocality
+    }
 }
 
 extension WeatherForecastView: UICollectionViewDelegate {
     
 }
 
-extension WeatherForecastView: UICollectionViewDataSource{
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
+extension WeatherForecastView: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return 40
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WeatherForecastCell.identifier, for: indexPath)
+        let cell: WeatherForecastCell = collectionView.dequeueReusableCell(withReuseIdentifier: WeatherForecastCell.identifier, for: indexPath) as! WeatherForecastCell
         
-        let title = UILabel(frame: CGRect(x: 0, y: 0, width: cell.bounds.size.width, height: 50))
-         title.text = "\(indexPath)"
-         title.textAlignment = .center
-         cell.addSubview(title)
+        if let date = fiveDaysWeatherModel?.list?[indexPath.row].dt {
+            let date = NSDate(timeIntervalSince1970: TimeInterval(date))
+            let dateFormatter = DateFormatter()
+            dateFormatter.locale = Locale(identifier:"ko_KR")
+            dateFormatter.dateFormat = "MM/dd(E) HH시"
+            let time = dateFormatter.string(from: date as Date)
+            cell.configure(date: time)
+        }
+        
+        if let temperatureCurrent = fiveDaysWeatherModel?.list?[indexPath.row].main?.temp {
+            cell.configure(temperatureCurrent: temperatureCurrent)
+        }
+        
+        if let image = UIImage(systemName: "square.and.arrow.up") {
+            cell.configure(image: image)
+        }
+        
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: WeatherForecastHeaderView.identifier, for: indexPath) as? WeatherForecastHeaderView else {
-            return UICollectionViewCell()
+            return UICollectionReusableView()
         }
-        if let image = UIImage(systemName: "square.and.arrow.up") {
-            header.configure(with: image)
+        
+        if let image = UIImage(systemName: "pencil") {
+            header.configure(image: image)
         }
+        
+        if let locality = placemark?.locality,
+           let subLocality = placemark?.subLocality {
+            header.configure(locality: locality, subLocality: subLocality)
+        }
+        
+        if let temperatureMin = currentWeathermodel?.main?.tempMin,
+           let temperatureMax = currentWeathermodel?.main?.tempMax,
+           let temperatureCurrent = currentWeathermodel?.main?.temp {
+            header.configure(temperatureMin: temperatureMin, temperatureMax: temperatureMax, temperatureCurrent: temperatureCurrent)
+        }
+        
         return header
     }
 }
-
-extension WeatherForecastView: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        return CGSize(width: collectionView.frame.width, height: 200)
-    }
-}
-
-#if DEBUG
-import SwiftUI
-struct ViewControllerRepresentable: UIViewControllerRepresentable {
-    
-    // update
-    func updateUIViewController(_ uiViewController: UIViewController, context: Context){
-        
-    }
-    // makeui
-    @available(iOS 13.0, *)
-    func makeUIViewController(context: Context) -> UIViewController {
-        WeatherForecastViewController()
-    }
-}
-
-struct ViewController_Previews: PreviewProvider {
-    static var previews: some View{
-        ViewControllerRepresentable().previewDisplayName("iPhone 15")
-    }
-}
-#endif
