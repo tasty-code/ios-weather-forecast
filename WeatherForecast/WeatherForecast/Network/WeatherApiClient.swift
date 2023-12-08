b6cfe2e7f55f0a4342b974d62a3c6d7b86f44073
dpwns1234 <52391722+dpwns1234@users.noreply.github.com>
2023-12-08 14:26:43 +0900

    refactor: weatherApiClient 리팩터
    - weatherApiClien와 weatherType을 enum type으로 합침

--- WeatherForecast/WeatherForecast/Network/WeatherApiClient.swift
+++ WeatherForecast/WeatherForecast/Network/WeatherApiClient.swift
@@ -4,30 +4,51 @@
 //
 //  Created by 김예준 on 11/21/23.
 //
-
+import Foundation
 import CoreLocation
 
-struct WeatherApiClient {
-    private static let baseURL = "https://api.openweathermap.org/data/2.5/"
+enum WeatherApiClient {
+    case forecast(coordinate: CLLocationCoordinate2D)
+    case today(coordinate: CLLocationCoordinate2D)
+    case icon(code: String)
     
-    static func makeURL(weatherType: WeatherType, coord: CLLocationCoordinate2D) -> URL? {
-        // let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String
-        let apiKey = "9025fdb78bf735a4b7287e0dcc03e4fd"
-        var components = URLComponents(string: baseURL)
-        components?.path += weatherType.path
-        
-        let latQueryItem = URLQueryItem(name: "lat", value: String(coord.latitude))
-        let lonQueryItem = URLQueryItem(name: "lon", value: String(coord.longitude))
-        let apiKeyQueryItem = URLQueryItem(name: "appid", value: apiKey)
-        components?.queryItems = [latQueryItem, lonQueryItem, apiKeyQueryItem]
-        
-        return components?.url
+    var urlPath: String {
+        switch self {
+        case .forecast:
+            return "forecast/"
+        case .today:
+            return "weather/"
+        case .icon:
+            return "img/wn/"
+        }
     }
     
-    static func makeRequest(weatherType: WeatherType, coord: CLLocationCoordinate2D) throws -> URLRequest {
-        guard let url = makeURL(weatherType: weatherType, coord: coord) else { throw NetworkError.invailedURL }
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        return request
+    var componenets: URLComponents? {
+        let components = URLComponents()
+        switch self {
+        case .forecast, .today:
+            return URLComponents(string: "https://api.openweathermap.org/data/2.5/\(self.urlPath)")
+        case .icon(code: let code):
+            return URLComponents(string: "https://openweathermap.org/\(self.urlPath)\(code)@2x.png")
+        }
+    }
+    
+    var queryItems: [URLQueryItem] {
+        let apiKey = "9025fdb78bf735a4b7287e0dcc03e4fd"
+        switch self {
+        case .forecast(coordinate: let coord), .today(coordinate: let coord):
+            let latQueryItem = URLQueryItem(name: "lat", value: String(coord.latitude))
+            let lonQueryItem = URLQueryItem(name: "lon", value: String(coord.longitude))
+            let apiKeyQueryItem = URLQueryItem(name: "appid", value: apiKey)
+            return [latQueryItem, lonQueryItem, apiKeyQueryItem]
+        default:
+            return []
+        }
+    }
+    
+    func makeURL() -> URL? {
+        var compoenets = self.componenets
+        compoenets?.queryItems = queryItems
+        return componenets?.url
     }
 }
