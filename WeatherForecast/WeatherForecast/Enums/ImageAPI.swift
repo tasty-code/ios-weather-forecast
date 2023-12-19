1ab387128f11d6419f66a00afc76a2deaee8277d
JJong <jfknfj920@naver.com>
2023-12-19 18:38:46 +0900

    chore: ImageAPI 내 iconType -> imageType으로 네이밍 변경

--- WeatherForecast/WeatherForecast/Enums/ImageAPI.swift
+++ WeatherForecast/WeatherForecast/Enums/ImageAPI.swift
@@ -2,14 +2,14 @@ import Foundation
 
 struct ImageAPI: Requestable {
     
-    private let iconType: String
+    private let imageType: String
     
-    init(iconType: String) {
-        self.iconType = iconType
+    init(imageType: String) {
+        self.imageType = imageType
     }
     
     var path: URL? {
-        let result: URL? = URL(string: "https://openweathermap.org/img/wn/\(iconType)@2x.png")
+        let result: URL? = URL(string: "https://openweathermap.org/img/wn/\(imageType)@2x.png")
         return result
     }
 }
